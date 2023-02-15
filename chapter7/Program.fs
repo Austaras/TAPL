5dfd07f21d9157cb5edc4158f5fdb18bb85bdb46
austaras <austaras@outlook.com>
2023-02-15 15:36:36 +0800

    fix and refactor

--- chapter7/Program.fs
+++ chapter7/Program.fs
@@ -1,4 +1,6 @@
-﻿type Apply = { arg: Term; callee: Term }
+﻿// Untyped Lambda-Calculus
+
+type Apply = { arg: Term; callee: Term }
 and Abs = { name: string; body: Term }
 
 and Term =
@@ -7,12 +9,15 @@ and Term =
     | Apply of Apply
 
 let pickfreshname name ctx =
-    let mutable x = 0
-    let mutable name = name
+    let rec pick_inner name ctx suffix =
+        let new_name = name + if suffix < 0 then "" else string suffix
 
-    while Array.exists (fun n -> n = name) ctx do
-        name <- name + string x
-        x <- x + 1
+        if Array.contains new_name ctx then
+            pick_inner name ctx (suffix + 1)
+        else
+            new_name
+
+    let name = pick_inner name ctx -1
 
     Array.append ctx [| name |], name
 
@@ -37,12 +42,17 @@ let to_string ctx term =
 
     to_string ctx term false
 
+printfn "Input: %s" (to_string [| "x" |] (Abs { name = "x"; body = Var 0 }))
+
 let ctx = [| "b" |]
 
 let term =
     Apply
-        { callee = Abs { name = "x"; body = Var 0 }
-          arg = Abs { name = "x"; body = Var 1 } }
+        { callee =
+            Apply
+                { callee = Abs { name = "x"; body = Var 0 }
+                  arg = Abs { name = "x"; body = Var 1 } }
+          arg = Abs { name = "x"; body = Var 0 } }
 
 printfn "Input: %s" (to_string ctx term)
 
@@ -84,16 +94,12 @@ let eval_call body arg =
 
     shift { d = -1; c = 0 } term
 
-let isval term =
-    match term with
-    | Abs _ -> true
-    | _ -> false
-
 let rec eval term =
     match term with
-    | Apply { callee = Abs callee; arg = arg } when isval arg -> eval_call callee.body arg
-    | Apply a when isval a.callee -> eval (Apply { callee = a.callee; arg = eval a.arg })
-    | Apply a -> eval (Apply { callee = eval a.callee; arg = a.arg })
+    | Apply { callee = Abs callee
+              arg = Abs _ as arg } -> eval_call callee.body arg |> eval
+    | Apply { callee = Abs _ as callee; arg = arg } -> Apply { callee = callee; arg = eval arg } |> eval
+    | Apply a -> Apply { callee = eval a.callee; arg = a.arg } |> eval
     | _ -> term
 
 printfn "Result small step: %s" (to_string ctx (eval term))
@@ -101,8 +107,8 @@ printfn "Result small step: %s" (to_string ctx (eval term))
 let rec eval_big_step term =
     match term with
     | Apply { callee = callee; arg = arg } ->
-        match (eval callee, eval arg) with
-        | (Abs callee, Abs arg) -> eval_call callee.body (Abs arg)
+        match (eval_big_step callee, eval_big_step arg) with
+        | (Abs callee, Abs arg) -> eval_call callee.body (Abs arg) |> eval_big_step
         | (_, _) -> term
     | _ -> term
 
